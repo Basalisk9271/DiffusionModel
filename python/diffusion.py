30bdc07b47d81c045b7a77226baf697d54a287d2
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-11-12 19:57:40 -0500

    Python File Updates and added Partition

--- python/diffusion.py
+++ python/diffusion.py
@@ -7,8 +7,10 @@ import math
 
 
 def main():
-    maxsize = 5
+    maxsize = int(input("What would you like maxsize to be: "))
+    choice = input("Would you like a partition (y for 'yes' and n for 'no'): ")
     cube = [[[0.0] * (maxsize) for _ in range(maxsize)] for _ in range(maxsize)]
+    partition = [[[0.0] * (maxsize) for _ in range(maxsize)] for _ in range(maxsize)]
     # Zero the cube
     i = 0
     while (i < maxsize) :
@@ -20,6 +22,7 @@ def main():
                 k += 1
             j += 1
         i += 1
+  
     diffusion_coefficient = 0.175
     room_dimension = 5 # 5 Meters
     speed_of_gas_molecules = 250.0 # Based on 100 g/mol gas at RT
@@ -27,9 +30,28 @@ def main():
     distance_between_blocks = room_dimension / maxsize
     DTerm = diffusion_coefficient * timestep / (distance_between_blocks * distance_between_blocks)
     
+    # Zero the partition
+    i = 0
+    while (i < maxsize) :
+        j = 0
+        while (j < maxsize) :
+            k = 0
+            while (k < maxsize) :
+                partition[i][j][k] = 0
+                k += 1
+            j += 1
+        i += 1
     # Initialize the first cell
 
     cube[0][0][0] = 1.0E21
+
+    for i in range((math.ceil(maxsize*.5)-1), (math.ceil(maxsize*.5))) :
+        for j in range (math.ceil(maxsize*.5)-1, maxsize) :
+            for k in range (0,maxsize) :
+                partition[i][j][k] = 0
+                
+    # Initialize the first cell
+
 
     time = 0.0 # to keep up with accumulated system time.
     ratio = 0.0
@@ -53,7 +75,12 @@ def main():
                         while (m < maxsize) :
                             n = 0 
                             while (n < maxsize) :
-                                if (((i == l) and (j == m) and (k == n + 1)) or ((i == l) and (j == m) and (k == n - 1)) or ((i == l) and (j == m + 1) and (k == n)) or ((i == l) and (j == m - 1) and (k == n)) or ((i == l + 1) and (j == m) and (k == n)) or ((i == l - 1) and (j == m) and (k == n))) :
+                                if ((   ((i == l) and (j == m) and (k == n + 1)) or 
+                                        ((i == l) and (j == m) and (k == n - 1)) or 
+                                        ((i == l) and (j == m + 1) and (k == n)) or 
+                                        ((i == l) and (j == m - 1) and (k == n)) or 
+                                        ((i == l + 1) and (j == m) and (k == n)) or 
+                                        ((i == l - 1) and (j == m) and (k == n))) and partition[i][j][k] != 1 and partition[l][m][n] != 1) :
                                     change = (cube[i][j][k] - cube[l][m][n]) * DTerm
                                     cube[i][j][k] = cube[i][j][k] - change
                                     cube[l][m][n] = cube[l][m][n] + change
@@ -76,9 +103,10 @@ def main():
             while (j < maxsize) :
                 k = 0
                 while (k < maxsize) :
-                    maxval = max(cube[i][j][k],maxval)
-                    minval = min(cube[i][j][k],minval)
-                    sumval += cube[i][j][k]
+                    if (partition[i][j][k] != 1):
+                        maxval = max(cube[i][j][k],maxval)
+                        minval = min(cube[i][j][k],minval)
+                        sumval += cube[i][j][k]
                     k += 1
                 j += 1
             i += 1
