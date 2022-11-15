bb1e893388c0c1d13c3d6a3873b4f8e516beb418
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-11-15 14:12:29 -0500

    Python File Updates

--- python/diffusion.py
+++ python/diffusion.py
@@ -48,17 +48,12 @@ def main():
      
     while True :
         i = 0
-        while (i < maxsize) :
-            j = 0
-            while (j < maxsize) :
-                k = 0 
-                while (k < maxsize) :
-                    l = 0 
-                    while (l < maxsize) :
-                        m = 0
-                        while (m < maxsize) :
-                            n = 0 
-                            while (n < maxsize) :
+        for i in range (0,maxsize) :
+            for j in range (0,maxsize) :
+                for k in range (0,maxsize) :
+                    for l in range (0,maxsize) :
+                        for m in range (0,maxsize) :
+                            for n in range (0,maxsize) :
                                 if ((   ((i == l) and (j == m) and (k == n + 1)) or 
                                         ((i == l) and (j == m) and (k == n - 1)) or 
                                         ((i == l) and (j == m + 1) and (k == n)) or 
@@ -68,12 +63,6 @@ def main():
                                     change = (cube[i][j][k] - cube[l][m][n]) * DTerm
                                     cube[i][j][k] = cube[i][j][k] - change
                                     cube[l][m][n] = cube[l][m][n] + change
-                                n += 1
-                            m += 1
-                        l += 1
-                    k += 1
-                j += 1
-            i += 1
         
         time = time + timestep
         
