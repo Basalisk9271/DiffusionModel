1bf90a418463257c62933ddf22f8e2f459125df0
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-11-15 18:52:24 -0500

    Julia File Updates

--- julia/diffusion.jl
+++ julia/diffusion.jl
@@ -1,4 +1,8 @@
 #!/usr2/local/julia-1.8.2/bin/julia
+#Gabe Imlay
+#CSC330: Organization of Programming Languages
+#Project 2: Diffusion Model -> Julia
+#November 17th, 2022
 
 function main()
     print("What would you like maxsize to be: ")
